f74668af939de60f3089c8de0bf4993a6f88f907
Jesse Gao <jesse@forinx.com>
2025-09-29 16:44:05 +0800

    Fontsize from 10 to 12

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -64,7 +64,7 @@ namespace DiskMonitor
             var sp = new StackPanel() { Margin = new Thickness(0, 0, 0, 2) };
 
             var bar = new ProgressBar() { Height = 13, Minimum = 0, Maximum = 100 };
-            var label = new Label() { Content = drive, FontSize = 10, Foreground = System.Windows.Media.Brushes.Black };
+            var label = new Label() { Content = drive, FontSize = 12, Foreground = System.Windows.Media.Brushes.Black };
 
             sp.Children.Add(label);
             sp.Children.Add(bar);
